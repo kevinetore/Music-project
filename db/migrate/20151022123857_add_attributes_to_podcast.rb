0b27605209230cca11bdb6e0e77f5f5d814195f8
Kevin Etore <info@etore.nl>
2015-10-22 14:49:28 +0200

    Added attributes to podcast model

---
+++ db/migrate/20151022123857_add_attributes_to_podcast.rb
@@ -0,0 +1,9 @@
+class AddAttributesToPodcast < ActiveRecord::Migration
+  def change
+    add_column :podcasts, :title, :string
+    add_column :podcasts, :description, :text
+    add_column :podcasts, :itunes, :string
+    add_column :podcasts, :podbay, :string
+    add_column :podcasts, :soundcloud, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022121548) do
+ActiveRecord::Schema.define(version: 20151022123857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(version: 20151022121548) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "title"
+    t.text     "description"
+    t.string   "itunes"
+    t.string   "podbay"
+    t.string   "soundcloud"
   end
 
   add_index "podcasts", ["email"], name: "index_podcasts_on_email", unique: true, using: :btree
