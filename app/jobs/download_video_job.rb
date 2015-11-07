d7777bfbe8c91fa19fb16a0d67ed938936b65d5f
sapslaj <saps.laj@gmail.com>
2015-11-07 12:01:08 -0500

    Added job

---
+++ app/jobs/download_video_job.rb
@@ -0,0 +1,15 @@
+class DownloadVideoJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(episode_id, video_url)
+    video = YoutubeDL::Video.new(video_url, output: Rails.root.join('tmp', 'uploads', '%(id)s.%(ext)s').to_s, extract_audio: true, audio_format: 'mp3')
+
+    video.download
+
+    # Using Paperclip::FileAdapter
+    Episode.find(episode_id).mp3 = File.new(video.filename)
+
+    # Remove temporary file.
+    FileUtils.rm(video.filename)
+  end
+end
