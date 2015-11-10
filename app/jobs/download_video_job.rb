46f419656d71334d7dd8160051c1da00cde0da05
Kevin Etore <info@etore.nl>
2015-11-10 23:20:53 +0100

    added styling to new episode youtube link still need fix on redirect after convertion is completed

--- app/jobs/download_video_job.rb
+++ app/jobs/download_video_job.rb
@@ -2,7 +2,7 @@ class DownloadVideoJob < ActiveJob::Base
   queue_as :default
 
   def perform(episode_id, video_url)
-    video = YoutubeDL::Video.new(video_url, output: Rails.root.join('tmp', 'uploads', '%(id)s.%(ext)s').to_s, extract_audio: true, audio_format: 'mp3')
+    video = YoutubeDL::Video.new(video_url, output: Rails.root.join('music', 'mp3', '%(title)s-%()s.%(ext)s').to_s, extract_audio: true, audio_format: 'mp3')
 
     video.download
 
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -7,8 +7,6 @@ class Episode < ActiveRecord::Base
   has_attached_file :mp3
   validates_attachment :mp3, content_type: { content_type: ["audio/mpeg", "audio/mp3"] }
 
-  # Hook into write_attribute to check if any YouTube proccessing needs to be done
-  # TODO: Find a better way to do this
   def write_attribute(attr_name, value)
     check_for_youtube_proccessing(read_attribute(attr_name), value) if attr_name.intern == :youtube_url
     super
